29640d94f53682e55f2b782884738984c5900894
cauantrapp@outlook.com <caua_trapp@estudante.sesisenai.org.br>
2025-03-05 09:19:38 -0300

    inicializando js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-a=1
+//pegar o elementos canvas pelo id
+const canvas = document.getElementById('jogo')
+// inicializar o canvas
+const ctx = canvas.getContext('2d')
+
+//criar funcão loop
+function loop(){
+
+}
+
+loop()
