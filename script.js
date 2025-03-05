39d91efd5fff5f26b3ba057a3492bc5f92e6c111
cauantrapp@outlook.com <caua_trapp@estudante.sesisenai.org.br>
2025-03-05 11:01:46 -0300

    criando personagem

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 //pegar o elementos canvas pelo id
 const canvas = document.getElementById('jogo')
-// inicializar o canvas
+// inicializar o canvas / ctx igual a contexto do canva
 const ctx = canvas.getContext('2d')
+const gravidade = 0.5
+
+document.addEventListener('keypress', (e) => {
+    if(e.code == 'Space' && personagem.pulando == false) {
+        console.log('clicou para pular')
+        personagem.velocidadey = 15
+        personagem.pulando = true
+    }
+})
+
+const personagem = {
+    x:100,
+    y:canvas.height - 50,
+    altura:50,
+    largura:50,
+    velocidadey:0,
+    pulando:false
+}
+function desenharPersonagem(){
+    ctx.fillStyle ='brown'
+    ctx.fillRect( personagem.x, personagem.y, personagem.altura, personagem.largura)
+}
+
+function atualizarPersonagem(){
+    if(personagem.pulando == true){
+        personagem.velocidadey -= gravidade
+        personagem.y -= personagem.velocidadey
+        if(personagem.y >= canvas.height-50){
+            personagem.velocidadey = 0
+            personagem.pulando = false
+            personagem.y = canvas.height - 50
+        }
+        
+    }
+
+
+}
+
 
 //criar funcão loop
 function loop(){
+   // apagar a tela anterior
+   ctx.clearRect(0,0,canvas.width, canvas.height)
+   // desenhar novamente
+   desenharPersonagem()
+   // atualizar posições
+   atualizarPersonagem()
 
+
+   // chamar o loop denovo
+   requestAnimationFrame(loop)
 }
 
 loop()
